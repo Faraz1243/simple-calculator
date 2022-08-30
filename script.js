c7b03678aecfe0aae4628765d9707fba7e5b2d06
Faraz Ali <farazaliasif786@gmail.com>
2022-08-30 02:38:37 -0700

    some-scripting

---
+++ script.js
@@ -0,0 +1,79 @@
+var num = document.querySelectorAll('.display-text');
+var opt=null;
+
+
+function btn_AC() {
+    num.innerHTML = "330";
+}
+
+function btn_BSC(){
+    document.getElementById["displayID"].innerHTML=  "123";
+}
+
+function btn_Mod(){
+
+}
+
+function btn_Div(){
+
+}
+
+function btn_Mul(){
+
+}
+
+function btn_Sub(){
+
+}
+
+function btn_Add(){
+
+}
+
+function btn_Equal(){
+
+}
+
+function btn_Dot(){
+
+}
+
+function btn_0(){
+
+}
+
+function btn_1(){
+
+}
+
+function btn_2(){
+
+}
+
+function btn_3(){
+
+}
+
+function btn_4(){
+
+}
+
+function btn_5(){
+
+}
+
+function btn_6(){
+
+}
+
+function btn_7(){
+
+}
+
+function btn_8(){
+
+}
+
+function btn_9(){
+
+}
